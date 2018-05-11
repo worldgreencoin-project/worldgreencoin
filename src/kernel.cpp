c71cab51bcb1d522283e7255343443ced768b170
Lun <lun1109@hotmail.com>
2018-05-11 13:03:33 +0800

    test build

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -5,7 +5,7 @@
 
 #include <boost/assign/list_of.hpp>
 #include <math.h>
-#include <string>
+#include <sstream>
 #include "kernel.h"
 #include "txdb.h"
 #include "script.h"
@@ -442,7 +442,9 @@ bool CheckStakeModifierCheckpoints(int nHeight, uint64_t nStakeModifierChecksum)
 {
     if (fDebug) {
         LogPrintf("CheckStakeModifierCheckpoints : nHeight=%d, nStakeModifierChecksum=0x%016x\n", nHeight, nStakeModifierChecksum);
-        LogPrintf("CheckStakeModifierCheckpoints : nHeight=%d, nStakeModifierChecksum=%s\n", nHeight, std::to_string(nStakeModifierChecksum));
+        std::ostringstream oss;
+        oss << nStakeModifierChecksum;
+        LogPrintf("CheckStakeModifierCheckpoints : nHeight=%d, nStakeModifierChecksum=%s\n", nHeight, oss.str());
     }
     
     MapModifierCheckpoints& checkpoints = TestNet() ? mapStakeModifierCheckpointsTestNet : mapStakeModifierCheckpoints;
